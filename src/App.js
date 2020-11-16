f0da113e8978bcb25ffdb5304b4c834da4fdab2f
Leonard <leonard.flachs@gmail.com>
2020-11-16 15:15:41 +0100

    :sparkles: add handle favorite function

--- src/App.js
+++ src/App.js
@@ -10,12 +10,40 @@ import Homepage from './Pages/Homepage';
 import ProductInfo from './Pages/ProductInfo';
 
 class App extends React.Component {
+	state = {
+		favorites: [],
+	};
+
+	handleFavorite(recipeItem) {
+		const isFavorite = () =>
+			this.state.favorites.some((recipe) => recipe.id === recipeItem.id);
+
+		if (isFavorite(recipeItem)) {
+			this.removeFavorite(recipeItem);
+		} else {
+			this.addFavorite(recipeItem);
+		}
+	}
+	removeFavorite(favoriteItem) {
+		this.setState({
+			favorites: this.state.favorites.filter(
+				(recipe) => recipe.id !== favoriteItem.id
+			),
+		});
+	}
+	addFavorite(favoriteItem) {
+		this.setState({ favorites: [...this.state.favorites, favoriteItem] });
+	}
 	render() {
 		return (
 			<Router>
+				<button onClick={() => this.handleFavorite({ id: 1, text: 'test' })}>
+					test
+				</button>
 				<Switch>
 					<Route path='/meals/:meal' exact component={ProductInfo} />
 					<Route path='/' exact component={Homepage} />
+					<Route path='/favorite' exact component={Homepage} />
 				</Switch>
 			</Router>
 		);
